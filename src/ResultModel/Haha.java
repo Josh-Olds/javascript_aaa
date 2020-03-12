a741f3236ff9067e2e6e1584087dd32e714609ac
GHQ <18719123749@.163.com>
2020-03-12 15:10:47 +0800

    哈哈

---
+++ src/ResultModel/Haha.java
@@ -0,0 +1,14 @@
+package ResultModel;
+
+/**
+ * @author Josh
+ * @version 1.0.0
+ * @ClassName Haha.java
+ * @Description TODO
+ * @createTime 2020-03-11 11:27:00
+ */
+public class Haha {
+    public static void main(String[] args) {
+
+    }
+}
