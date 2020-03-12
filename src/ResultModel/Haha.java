1cae203838ab11f44ca83ea36b8fd71b110f4caa
GHQ <18719123749@.163.com>
2020-03-12 15:22:31 +0800

    第二次提交

--- src/ResultModel/Haha.java
+++ src/ResultModel/Haha.java
@@ -3,12 +3,10 @@ package ResultModel;
 /**
  * @author Josh
  * @version 1.0.0
- * @ClassName Haha.java
- * @Description TODO
- * @createTime 2020-03-11 11:27:00
  */
 public class Haha {
     public static void main(String[] args) {
 
+        System.out.println("hahha");
     }
 }
