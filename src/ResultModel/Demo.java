121a6571aa69f1f4da122222621753adf1bab6e7
Josh <josh_mr@163.com>
2020-03-18 17:08:19 +0800

    demo frist committed

---
+++ src/ResultModel/Demo.java
@@ -0,0 +1,21 @@
+package ResultModel;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+/**
+ * @author Josh
+ * @version 1.0.0
+ * @ClassName Demo.java
+ * @Description TODO
+ * @createTime 2020-03-18 17:04:00
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hahah");
+        System.out.println("hahah");
+        System.out.println("hello world ");
+    }
+}
