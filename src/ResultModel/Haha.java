bad3c040cef33b2fd16c03177d1784afb2b50b83
Josh <josh_mr@163.com>
2020-03-18 16:23:39 +0800

    哈哈

--- src/ResultModel/Haha.java
+++ src/ResultModel/Haha.java
@@ -8,10 +8,18 @@ public class Haha {
     public static void main(String[] args) {
 
 
+
+        System.out.println("hahhehe ");
+        System.out.println("josh");
+        System.out.println("hahhehe ");
+        System.out.println("josh");
         System.out.println("hahhehe ");
         System.out.println("josh");
         System.out.println("hahha");
         System.out.println("joahsshfsd");
+        System.out.println("hahhehe ");
+        System.out.println("hahhehe ");
+        System.out.println("hahhehe ");
     }
 
 
